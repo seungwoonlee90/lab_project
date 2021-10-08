6078b1016c50c02982fa8a9f004e64738c9e9474
ethanlee <seungwoonlee90@gmail.com>
2021-10-08 10:02:59 +0900

    Update : smallhydro obscd_df

--- bigdatacenter/smallhydro_energy/app.py
+++ bigdatacenter/smallhydro_energy/app.py
@@ -1,10 +1,23 @@
 import os
 import pandas as pd
 import requests
+from datetime import datetime
 
 apiKey = os.environ.get("SECURE")
-obs = pd.read_csv('obscd.csv', encoding='cp949')
-obscds = obs['obscd'].to_list()
+year = datetime.today().year
+
+obscds =[]
+for j in range(1,6) :
+    url = f'http://www.wamis.go.kr:8080/wamis/openapi/wkw/wl_dubwlobs?basin={j}&oper=y&output=json&key={apiKey}'
+    res = requests.get(url)
+    data = res.json()
+
+    for i in range(data['count']) :
+        obscds.append([data['list'][i]['obscd'], data['list'][i]['obsnm'], data['list'][i]['sbsncd'],
+                     data['list'][i]['mngorg']])
+
+obscd_df = pd.DataFrame(obscds, columns= ['obscd', 'obsnm', 'sbsncd', 'mngorg'])
+obscds = obscd_df['obscd'].to_list()
 
 df = []
 
@@ -22,7 +35,7 @@ print('수집 완료')
 df = pd.DataFrame(df, columns= ['obscd', 'ymd', 'fw'])
 df['fw'] = df['fw'].apply(lambda x : '-999' if (x == '-') else x)
 df['fw'] = df['fw'].astype(float)
-res = pd.merge(obs, df, on='obscd', how='outer')
+res = pd.merge(obscd_df, df, on='obscd', how='outer')
 
 with open('./samllhydro.json', 'a', encoding='utf-8') as file:
     res.to_json(file, force_ascii=False, orient='records')
