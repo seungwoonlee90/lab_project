15b8d848ddc70e02dd97f36df9e591442b9f57bc
ethanlee <seungwoonlee90@gmail.com>
2021-10-08 13:20:13 +0900

    Update : smallhydro.json

--- bigdatacenter/smallhydro_energy/main.js
+++ bigdatacenter/smallhydro_energy/main.js
@@ -2,7 +2,6 @@ let start = document.getElementById('start');
 let fw = document.querySelectorAll('span');
 let datetime = document.getElementById('datetime');
 let section = document.querySelector('section');
-
 let date = new Date().toLocaleDateString()
 
 window.onload = function(){
@@ -11,103 +10,20 @@ window.onload = function(){
 
 start.addEventListener('click', function(event) {
     event.preventDefault();
-    const apiKey = document.getElementById('apiKey').value;
-    api_call(apiKey);
+    api_call();
 });
 
-function api_call(apiKey) {
-    for(k = 1 ; k < 6 ; k++) {
-        let url = `http://www.wamis.go.kr:8080/wamis/openapi/wkw/flw_dubobsif?&basin=${k}&oper=y&output=json&key=${apiKey}`
-
-        axios.request({
-            method: 'GET',
-            url: url,
-            headers: {'Content-Type': 'application/json'},
-            type: 'json'
+function api_call() {
+       axios.request({
+           url :'https://raw.githubusercontent.com/seungwoonlee90/lab_project/master/bigdatacenter/smallhydro_energy/samllhydro.json',
+           headers: {'Content-Type': 'application/json'},
+           type : 'json'
         })
-        .then(function(res) {
-
-            for(i = 0 ; i < res.data.count ; i ++) {
-                let obscd = res.data.list[i].obscd;
-                let obsnm = res.data.list[i].obsnm;
-                let url = `http://www.wamis.go.kr:8080/wamis/openapi/wkw/flw_dtdata?obscd=${obscd}&year=2021&output=json&key=${apiKey}`
-
-                axios.request({
-                    method: 'GET',
-                    url: url,
-                    headers: {'Content-Type': 'application/json'},
-                    type:'json'
-                }).then(function(response) {
-                    let data = response.data.list[response.data.list.length - 2]
-                    const effi = document.getElementById('ef').value;
-                    if(!data) {
-                        let ymd_ = document.createElement('span')
-                        let obscd_ = document.createElement('span')
-                        let obsnm_ = document.createElement('span')
-                        let fw_ = document.createElement('span')
-                        let power_ = document.createElement('span')
-
-                        ymd_.innerHTML = -999;
-                        obscd_.innerHTML = obscd;
-                        obsnm_.innerHTML = obsnm;
-                        fw_.innerHTML = -999;
-                        power_.innerHTML = -999;
-                        
-                        section.appendChild(ymd_);
-                        section.appendChild(obscd_);
-                        section.append(obsnm_)
-                        section.appendChild(fw_);
-                        section.appendChild(power_)
-
-                        ymd_.style.color = "red"
-                        obscd_.style.color = "red"
-                        obsnm_.style.color = "red"
-                        fw_.style.color = "red"
-                        power_.style.color = "red"
-
-                    } else {
-
-                        let ymd_ = document.createElement('span')
-                        let obscd_ = document.createElement('span')
-                        let obsnm_ = document.createElement('span')
-                        let fw_ = document.createElement('span')
-                        let power_ = document.createElement('span')
-    
-                        let fw = data.fw.replace(/-/gi, '-999')
-                        if(fw>0 & fw<1) {
-                            fw = `0${fw}`
-                        }
-
-                        let ymd = data.ymd
-
-                        ymd_.innerHTML = ymd;
-                        obscd_.innerHTML = obscd;
-                        obsnm_.innerHTML = obsnm;
-                        fw_.innerHTML = fw;
-    
-                        if(fw < 0) {
-                            power_.innerHTML = -999;
-                            ymd_.style.color = "red"
-                            obscd_.style.color = "red"
-                            obsnm_.style.color = "red"
-                            fw_.style.color = 'red';
-                            power_.style.color = 'red';
-                        } else {
-                            power_.innerHTML = Math.round(9.8 * fw * 1.5 * effi)
-                        }
-                        
-                        section.appendChild(ymd_);
-                        section.appendChild(obscd_);
-                        section.append(obsnm_)
-                        section.appendChild(fw_);
-                        section.appendChild(power_)
-                    }
-                })
-    
-            }
-        }
-        )
-    }
+       .then((response) => {
+        let data = response.data;
+        const effi = document.getElementById('ef').value;
+        console.log(typeof(data), data, effi)
+       })
 }
 
 let download = document.getElementById('download');
