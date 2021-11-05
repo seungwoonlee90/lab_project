cc9f2817e0a384ec62119f9376dac6da6aeb6513
ethanlee <seungwoonlee90@gmail.com>
2021-11-05 09:57:14 +0900

    Update : asyncio

--- bigdatacenter/smallhydro_energy/app.py
+++ bigdatacenter/smallhydro_energy/app.py
@@ -1,42 +1,67 @@
 import os
-import pandas as pd
-import requests
+import json
+from aiohttp import ClientSession
+import asyncio
+import time
 from datetime import datetime
+import pandas as pd
 
 apiKey = os.environ.get("SECURE")
 year = datetime.today().year
 
-obscds =[]
-for j in range(1,6) :
-    url = f'http://www.wamis.go.kr:8080/wamis/openapi/wkw/wl_dubwlobs?basin={j}&oper=y&output=json&key={apiKey}'
-    res = requests.get(url)
-    data = res.json()
 
-    for i in range(data['count']) :
-        obscds.append([data['list'][i]['obscd'], data['list'][i]['obsnm'], data['list'][i]['sbsncd'],
-                     data['list'][i]['mngorg']])
+async def fetcher(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
-obscd_df = pd.DataFrame(obscds, columns= ['obscd', 'obsnm', 'sbsncd', 'mngorg'])
-obscds = obscd_df['obscd'].to_list()
 
-df = []
+async def results(result, count):
+    res = json.loads(result[count])
+    res_count = res['count']
+    obscds = [[res['list'][k]['obscd'], res['list'][k]['obsnm'], res['list']
+               [k]['sbsncd'], res['list'][k]['mngorg']]for k in range(res_count)]
+    return obscds
 
-for obscd in obscds :
-    url = f'http://www.wamis.go.kr:8080/wamis/openapi/wkw/flw_dtdata?obscd={obscd}&year=2021&output=json&key={apiKey}'
-    res = requests.get(url)
-    data = res.json()
-    if data['count'] != 0 :
-        df.append([obscd, data['list'][-1]['ymd'], data['list'][-1]['fw']])
-    else :
+
+async def main(apiKey):
+    urls = [
+        f'http://www.wamis.go.kr:8080/wamis/openapi/wkw/wl_dubwlobs?basin={j}&oper=y&output=json&key={apiKey}' for j in range(1, 6)]
+
+    async with ClientSession() as session:
+        result = await asyncio.gather(*[fetcher(session, url) for url in urls])
+        datas = await asyncio.gather(*[results(result, count) for count in range(5)])
+        df = pd.DataFrame([y for x in datas for y in x], columns=[
+                          'obscd', 'obsnm', 'sbsncd', 'mngorg'])
+        return df
+
+
+async def hydro(result):
+    try:
+        res = json.loads(result)['list'][-1]
+        return res
+    except:
         pass
-    
-print('수집 완료')
 
-df = pd.DataFrame(df, columns= ['obscd', 'ymd', 'fw'])
-res = pd.merge(obscd_df, df, on='obscd', how='outer')
-res = res.fillna('-999')
-res['fw'] = res['fw'].apply(lambda x : '-999' if (x == '-') else x)
-res['fw'] = res['fw'].astype(float)
+
+async def smallhydro(obscds, apiKey):
+    urls = [
+        f'http://www.wamis.go.kr:8080/wamis/openapi/wkw/flw_dtdata?obscd={obscd}&year={year}&output=json&key={apiKey}' for obscd in obscds]
+    async with ClientSession() as session:
+        results = await asyncio.gather(*[fetcher(session, url) for url in urls])
+        result = await asyncio.gather(*[hydro(result)for result in results])
+        return pd.DataFrame(obscds, columns=['obscd']).join(pd.DataFrame([i if i is not None else {
+            'ymd': '-999', 'fw': '-999'} for i in result]))
+
+
+if __name__ == "__main__":
+    start = time.time()
+    base = asyncio.run(main(apiKey))
+    df = asyncio.run(smallhydro(base['obscd'].to_list(), apiKey))
+    res = pd.merge(base, df, on='obscd', how='outer')
+    res['fw'] = res['fw'].apply(lambda x: '-999' if (x == '-') else x)
+    res['fw'] = res['fw'].astype(float)
+    print(res)
+    print(time.time() - start)
 
 with open('./samllhydro.json', 'a', encoding='utf-8') as file:
     res.to_json(file, force_ascii=False, orient='records')
--- bigdatacenter/smallhydro_energy/main.js
+++ bigdatacenter/smallhydro_energy/main.js
@@ -15,14 +15,49 @@ start.addEventListener('click', function(event) {
 
 function api_call() {
        axios.request({
-           url :'https://raw.githubusercontent.com/seungwoonlee90/lab_project/master/bigdatacenter/smallhydro_energy/samllhydro.json',
+           url :'https://raw.githubusercontent.com/seungwoonlee90/lab_project/master/bigdatacenter/smallhydro_energy/smallhydro.json',
            headers: {'Content-Type': 'application/json'},
            type : 'json'
         })
        .then((response) => {
-        let data = response.data;
+        let data = new Array(response.data);
         const effi = document.getElementById('ef').value;
-        console.log(typeof(data), data, effi)
+        console.log(data[0][0])
+
+        for (let i = 0; i < 802; i+=1) {
+            let ymd = data[0][i]['ymd'];
+            let sbsncd = data[0][i]['sbsncd'];
+            let obscd =  data[0][i]['obscd'];
+            let obsnm = data[0][i]['obsnm'];
+            let fw = data[0][i]['fw'];
+            let pw = fw * 9.8 * effi * 1.5;
+
+            if (pw < 0 ) {
+                pw = '-999';
+            }
+
+            let ymd_ = document.createElement('span')
+            let sbsncd_ = document.createElement('span')
+            let obscd_ = document.createElement('span')
+            let obsnm_ = document.createElement('span')
+            let fw_ = document.createElement('span')
+            let pw_ = document.createElement('span')
+
+            ymd_.innerHTML = ymd
+            sbsncd_.innerHTML = sbsncd
+            obscd_.innerHTML = obscd
+            obsnm_.innerHTML = obsnm
+            fw_.innerHTML = fw
+            pw_.innerHTML = Math.round(pw)
+
+            section.appendChild(ymd_);
+            section.appendChild(sbsncd_);
+            section.appendChild(obscd_);
+            section.appendChild(obsnm_);
+            section.appendChild(fw_);
+            section.appendChild(pw_);
+        }
+
        })
 }
 
@@ -38,14 +73,16 @@ function saveCSV(){
     let datas = [];
     let array = {};
 
-    for (let i = 0; i < rows.length; i+=5) {
+    for (let i = 0; i < rows.length; i+=6) {
 
         let ymd = rows[i].innerHTML;
-        let obscd = rows[i+1].innerHTML;
-        let fw = rows[i+3].innerHTML;
-        let power = rows[i+4].innerHTML;
+        let sbsncd = rows[i+1].innerHTML;
+        let obscd = rows[i+2].innerHTML;
+        let obsnm = rows[i+3].innerHTML;
+        let q = rows[i+4].innerHTML;
+        let power = rows[i+5].innerHTML;
 
-        array = {ymd, obscd, fw, power}
+        array = {ymd, sbsncd, obscd, obsnm, q, power}
         datas.push(array);
     }
 
